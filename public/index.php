e89a8cbec5ff05c4ebeafad4072145bb363e7029
Aslan <autlevaslan@yandex.ru>
2022-06-21 10:34:21 +0300

    fix2: add Answer in bot

--- public/index.php
+++ public/index.php
@@ -7,7 +7,15 @@ $data = json_decode(file_get_contents("php://input"), true);
 $message = $data['message'];
 error_log(json_encode($data));
 
-sendAnswer('sendMessage', ['chat_id' => $message['chat']['id'],'text' => 'Вот Мой Ответ!']);
+sendAnswer('sendMessage', [
+    'chat_id' => $message['chat']['id'],
+    'text' => 'Вот Мой Ответ! 😁'
+]);
+
+sendAnswer('sendMessage', [
+    'chat_id' => $message['chat']['id'],
+    'text' => 'Вот мой ответ!' . hex2bin('F09F9882')
+]);
 
 function sendAnswer($method, $data, $headers = [])
 {
@@ -16,7 +24,7 @@ function sendAnswer($method, $data, $headers = [])
         CURLOPT_POST => 1,
         CURLOPT_HEADER => 0,
         CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => 'https://api.telegram.org/bot5308802884:AAHN1BguDHt_LB06OLsU0e8hav88dmIj1kY/setWebhook?url=https://whispering-thicket-18186.herokuapp.com/' . $method,
+        CURLOPT_URL => 'https://api.telegram.org/bot5308802884:AAHN1BguDHt_LB06OLsU0e8hav88dmIj1kY/' . $method,
         CURLOPT_POSTFIELDS => json_encode($data),
         CURLOPT_HTTPHEADER => array_merge(['Content-type: application/json'], $headers)
     ]);
