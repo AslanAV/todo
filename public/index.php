1d73c71e8bd569ae7c4a980ac4b2b7220c6fef7b
Aslan <autlevaslan@yandex.ru>
2022-06-21 12:07:10 +0300

    fix index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@ require "../vendor/autoload.php";
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-$app = new \App\App();
+$app = new \App\App(new \App\Transport());
 
 $app->index($data['message'], $data['message']['chat']['Id']);
