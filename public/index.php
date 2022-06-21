bae504f4d6478126f985a77b42a1b0ea5c19f0b9
Aslan <autlevaslan@yandex.ru>
2022-06-21 10:20:04 +0300

    add Answer in bot

--- public/index.php
+++ public/index.php
@@ -4,4 +4,23 @@ require "../vendor/autoload.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
 
+$message = $data['message'];
 error_log(json_encode($data));
+
+sendAnswer('sendMessage', ['chat_id' => $message['chat']['id'],'text' => 'Вот Мой Ответ!']);
+
+function sendAnswer($method, $data, $headers = [])
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, options: [
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://api.telegram.org/bot5308802884:AAHN1BguDHt_LB06OLsU0e8hav88dmIj1kY/setWebhook?url=https://whispering-thicket-18186.herokuapp.com/',
+        CURLOPT_POSTFIELDS => json_encode($data),
+        CURLOPT_HTTPHEADER => array_merge(['Content-type: application/json'],$headers)
+    ]);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($result, 1) ?? $result;
+}
